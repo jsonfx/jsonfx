87fe1b82ef3d2a1480c86ff762f2eb8f1b2825a4
Stephen M. McKamey <stephen@mckamey.com>
2010-05-31 20:31:12 +0000

    - build tweaks

--- Build.bat
+++ Build.bat
@@ -3,14 +3,14 @@ PUSHD "%~dp0"
 
 SET MSBuild=%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
 IF NOT EXIST "%MSBuild%" (
-	ECHO .NET Framework 4.0 is used to produce the various builds including for v2.0 and v3.5
+	ECHO Installation of .NET Framework 4.0 is required to build this project, including for .NET v2.0 and v3.5
 	ECHO http://www.microsoft.com/downloads/details.aspx?FamilyID=0a391abd-25c1-4fc0-919f-b21f31ab88b7
 	START /d "~\iexplore.exe" http://www.microsoft.com/downloads/details.aspx?FamilyID=0a391abd-25c1-4fc0-919f-b21f31ab88b7
 	EXIT /b 1
-	GOTO ERROR
+	GOTO END
 )
 
 FOR %%i IN (v2.0 v3.5 v4.0) DO "%MSBuild%" JsonFx2.Json.sln /target:rebuild /property:TargetFrameworkVersion=%%i;Configuration=Release
 
-:ERROR
+:END
 POPD
