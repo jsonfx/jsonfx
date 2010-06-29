ca701aeac3f9abf88edca7b2dd1000cf6143a33f
Stephen M. McKamey <stephen@mckamey.com>
2010-06-29 18:36:20 +0000

    - adding start of JsonFormatter tests - adding passing basic input edge case tests

---
+++ src/JsonFx.Json.Tests/Json/JsonFormatterTests.cs
@@ -0,0 +1,87 @@
+﻿#region License
+/*---------------------------------------------------------------------------------*\
+
+	Distributed under the terms of an MIT-style license:
+
+	The MIT License
+
+	Copyright (c) 2006-2010 Stephen M. McKamey
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
+
+\*---------------------------------------------------------------------------------*/
+#endregion License
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using JsonFx.Serialization;
+using Xunit;
+using Assert=JsonFx.AssertPatched;
+
+namespace JsonFx.Json
+{
+	public class JsonFormatterTests
+	{
+		#region Input Edge Case Tests
+
+		[Fact]
+		public void Format_EmptyInput_ReturnsEmptyString()
+		{
+			var formatter = new JsonWriter.JsonFormatter(new DataWriterSettings());
+
+			var expected = String.Empty;
+
+			var actual = formatter.Format(Enumerable.Empty<Token<JsonTokenType>>());
+
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void Format_NullInput_ThrowsArgumentNullException()
+		{
+			var formatter = new JsonWriter.JsonFormatter(new DataWriterSettings());
+
+			ArgumentNullException ex = Assert.Throws<ArgumentNullException>(
+				delegate
+				{
+					var actual = formatter.Format(null);
+				});
+
+			// verify exception is coming from expected param
+			Assert.Equal("tokens", ex.ParamName);
+		}
+
+		[Fact]
+		public void GetTokens_NullSettings_ThrowsArgumentNullException()
+		{
+			ArgumentNullException ex = Assert.Throws<ArgumentNullException>(
+				delegate
+				{
+					var formatter = new JsonWriter.JsonFormatter(null);
+				});
+
+			// verify exception is coming from expected param
+			Assert.Equal("settings", ex.ParamName);
+		}
+
+		#endregion Input Edge Case Tests
+	}
+}
