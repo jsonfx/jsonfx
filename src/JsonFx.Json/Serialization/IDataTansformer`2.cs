a916fb15745a6c3e04fa699eef6accdac76735a6
Stephen M. McKamey <stephen@mckamey.com>
2010-07-03 05:51:49 +0000

    - adding interface for token transformation

---
+++ src/JsonFx.Json/Serialization/IDataTansformer`2.cs
@@ -0,0 +1,54 @@
+#region License
+/*---------------------------------------------------------------------------------*\
+
+	Distributed under the terms of an MIT-style license:
+
+	The MIT License
+
+	Copyright (c) 2006-2010 Stephen M. McKamey
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
+
+\*---------------------------------------------------------------------------------*/
+#endregion License
+
+using System;
+using System.Collections.Generic;
+
+namespace JsonFx.Serialization
+{
+	/// <summary>
+	/// Consumes a SAX-like sequence of tokens and produces a token sequence of a different type
+	/// </summary>
+	/// <typeparam name="T">input token type</typeparam>
+	/// <typeparam name="T">output token type</typeparam>
+	public interface IDataTansformer<TIn, TOut>
+	{
+		#region Methods
+
+		/// <summary>
+		/// Transforms the token sequence from <typeparamref name="TIn"/> to <typeparamref name="TOut"/>
+		/// </summary>
+		/// <param name="tokens"></param>
+		/// <returns></returns>
+		IEnumerable<Token<TOut>> Transform(IEnumerable<Token<TIn>> tokens);
+
+		#endregion Methods
+	}
+}
