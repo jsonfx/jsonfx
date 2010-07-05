45f7f247f091c199bd1a2ccabe50ace49160989d
Stephen M. McKamey <stephen@mckamey.com>
2010-07-05 21:24:48 +0000

    - adding unit tests for MSAjaxDateFilter - fixing MSAjaxDateFilter bugs

---
+++ src/JsonFx.Json.Tests/Json/Filters/MSAjaxDateFilterTests.cs
@@ -0,0 +1,345 @@
+﻿#region License
+/*---------------------------------------------------------------------------------*\
+
+	Distributed under the terms of an MIT-style license:
+
+	The MIT License
+
+	Copyright (c) 2006-2010 Stephen M. McKamey
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
+
+\*---------------------------------------------------------------------------------*/
+#endregion License
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using JsonFx.Serialization;
+using Xunit;
+using Assert=JsonFx.AssertPatched;
+
+namespace JsonFx.Json.Filters
+{
+	public class MSAjaxDateFilterTests
+	{
+		#region TryRead Tests
+
+		[Fact]
+		public void TryRead_StandardTimeZone_ReadsAsUtc()
+		{
+			var expected = new DateTime(2008, 2, 29, 23, 59, 59, 999, DateTimeKind.Utc);
+
+			var input = new[]
+				{
+					JsonGrammar.TokenString(@"\/Date(1204329599999)\/")
+				};
+
+			DateTime actual;
+			Assert.True(new MSAjaxDateFilter().TryRead(new DataReaderSettings(), input, out actual));
+
+			Assert.Equal(expected.Kind, actual.Kind);
+			Assert.Equal(expected.Ticks, actual.Ticks);
+		}
+
+		[Fact]
+		public void TryRead_DaylightSavingsTimeZone_ReadsAsUtc()
+		{
+			var expected = new DateTime(2010, 7, 5, 10, 51, 17, 768, DateTimeKind.Utc);
+
+			var input = new[]
+				{
+					JsonGrammar.TokenString(@"\/Date(1278327077768)\/")
+				};
+
+			DateTime actual;
+			Assert.True(new MSAjaxDateFilter().TryRead(new DataReaderSettings(), input, out actual));
+
+			Assert.Equal(expected.Kind, actual.Kind);
+			Assert.Equal(expected.Ticks, actual.Ticks);
+		}
+
+		[Fact]
+		public void TryRead_FutureTimeZone_ReadsAsUtc()
+		{
+			var expected = new DateTime(2099, 12, 31, 23, 59, 59, 999, DateTimeKind.Utc);
+
+			var input = new[]
+				{
+					JsonGrammar.TokenString(@"\/Date(4102444799999)\/")
+				};
+
+			DateTime actual;
+			Assert.True(new MSAjaxDateFilter().TryRead(new DataReaderSettings(), input, out actual));
+
+			Assert.Equal(expected.Kind, actual.Kind);
+			Assert.Equal(expected.Ticks, actual.Ticks);
+		}
+
+		[Fact]
+		public void TryRead_DateTimeMinValueUtc_ReadsAsDateTimeMinValue()
+		{
+			var input = new[]
+				{
+					JsonGrammar.TokenString(@"\/Date(-62135596800000)\/")
+				};
+
+			var expected = DateTime.MinValue;
+
+			DateTime actual;
+			Assert.True(new MSAjaxDateFilter().TryRead(new DataReaderSettings(), input, out actual));
+
+			Assert.Equal(expected.Kind, actual.Kind);
+			Assert.Equal(expected.Ticks, actual.Ticks);
+		}
+
+		[Fact]
+		public void TryRead_DateTimeMaxValueUtc_ReadsAsDateTimeMaxValue()
+		{
+			var input = new[]
+				{
+					JsonGrammar.TokenString(@"\/Date(253402300800000)\/")
+				};
+
+			var expected = DateTime.MaxValue;
+
+			DateTime actual;
+			Assert.True(new MSAjaxDateFilter().TryRead(new DataReaderSettings(), input, out actual));
+
+			Assert.Equal(expected.Kind, actual.Kind);
+			Assert.Equal(expected.Ticks, actual.Ticks);
+		}
+
+		#endregion TryRead Tests
+
+		#region TryWrite Tests
+
+		[Fact]
+		public void TryWrite_UtcStandardTimeZone_DirectlyWritesAsUtc()
+		{
+			var input = new DateTime(2008, 2, 29, 23, 59, 59, 999, DateTimeKind.Utc);
+
+			var expected = new[]
+				{
+					JsonGrammar.TokenString(@"\/Date(1204329599999)\/")
+				};
+
+			IEnumerable<Token<JsonTokenType>> actual;
+			Assert.True(new MSAjaxDateFilter().TryWrite(new DataWriterSettings(), input, out actual));
+
+			Assert.NotNull(actual);
+			Assert.Equal(expected.Count(), actual.Count());
+			Assert.Equal(expected[0].TokenType, actual.First().TokenType);
+			Assert.Equal(expected[0].Value, actual.First().Value);
+		}
+
+		[Fact]
+		public void TryWrite_LocalStandardTimeZone_ConvertsToUtcAndWritesAsUtc()
+		{
+			var input = new DateTime(2008, 2, 29, 23, 59, 59, 999, DateTimeKind.Local);
+
+			var expected = new[]
+				{
+					JsonGrammar.TokenString(@"\/Date(1204358399999)\/")
+				};
+
+			IEnumerable<Token<JsonTokenType>> actual;
+			Assert.True(new MSAjaxDateFilter().TryWrite(new DataWriterSettings(), input, out actual));
+
+			Assert.NotNull(actual);
+			Assert.Equal(expected.Count(), actual.Count());
+			Assert.Equal(expected[0].TokenType, actual.First().TokenType);
+			Assert.Equal(expected[0].Value, actual.First().Value);
+		}
+
+		[Fact]
+		public void TryWrite_UnspecifiedStandardTimeZone_DirectlyWritesAsUtc()
+		{
+			var input = new DateTime(2008, 2, 29, 23, 59, 59, 999, DateTimeKind.Unspecified);
+
+			var expected = new[]
+				{
+					JsonGrammar.TokenString(@"\/Date(1204329599999)\/")
+				};
+
+			IEnumerable<Token<JsonTokenType>> actual;
+			Assert.True(new MSAjaxDateFilter().TryWrite(new DataWriterSettings(), input, out actual));
+
+			Assert.NotNull(actual);
+			Assert.Equal(expected.Count(), actual.Count());
+			Assert.Equal(expected[0].TokenType, actual.First().TokenType);
+			Assert.Equal(expected[0].Value, actual.First().Value);
+		}
+
+		[Fact]
+		public void TryWrite_UtcDaylightSavingsTimeZone_DirectlyWritesAsUtc()
+		{
+			var input = new DateTime(2010, 7, 5, 10, 51, 17, 768, DateTimeKind.Utc);
+
+			var expected = new[]
+				{
+					JsonGrammar.TokenString(@"\/Date(1278327077768)\/")
+				};
+
+			IEnumerable<Token<JsonTokenType>> actual;
+			Assert.True(new MSAjaxDateFilter().TryWrite(new DataWriterSettings(), input, out actual));
+
+			Assert.NotNull(actual);
+			Assert.Equal(expected.Count(), actual.Count());
+			Assert.Equal(expected[0].TokenType, actual.First().TokenType);
+			Assert.Equal(expected[0].Value, actual.First().Value);
+		}
+
+		[Fact]
+		public void TryWrite_LocalDaylightSavingsTimeZone_ConvertsToUtcAndWritesAsUtc()
+		{
+			var input = new DateTime(2010, 7, 5, 10, 51, 17, 768, DateTimeKind.Local);
+
+			var expected = new[]
+				{
+					JsonGrammar.TokenString(@"\/Date(1278352277768)\/")
+				};
+
+			IEnumerable<Token<JsonTokenType>> actual;
+			Assert.True(new MSAjaxDateFilter().TryWrite(new DataWriterSettings(), input, out actual));
+
+			Assert.NotNull(actual);
+			Assert.Equal(expected.Count(), actual.Count());
+			Assert.Equal(expected[0].TokenType, actual.First().TokenType);
+			Assert.Equal(expected[0].Value, actual.First().Value);
+		}
+
+		[Fact]
+		public void TryWrite_UnspecifiedDaylightSavingsTimeZone_DirectlyWritesAsUtc()
+		{
+			var input = new DateTime(2010, 7, 5, 10, 51, 17, 768, DateTimeKind.Unspecified);
+
+			var expected = new[]
+				{
+					JsonGrammar.TokenString(@"\/Date(1278327077768)\/")
+				};
+
+			IEnumerable<Token<JsonTokenType>> actual;
+			Assert.True(new MSAjaxDateFilter().TryWrite(new DataWriterSettings(), input, out actual));
+
+			Assert.NotNull(actual);
+			Assert.Equal(expected.Count(), actual.Count());
+			Assert.Equal(expected[0].TokenType, actual.First().TokenType);
+			Assert.Equal(expected[0].Value, actual.First().Value);
+		}
+
+		[Fact]
+		public void TryWrite_FutureUtcTimeZone_DirectlyWritesAsUtc()
+		{
+			var input = new DateTime(2099, 12, 31, 23, 59, 59, 999, DateTimeKind.Utc);
+
+			var expected = new[]
+				{
+					JsonGrammar.TokenString(@"\/Date(4102444799999)\/")
+				};
+
+			IEnumerable<Token<JsonTokenType>> actual;
+			Assert.True(new MSAjaxDateFilter().TryWrite(new DataWriterSettings(), input, out actual));
+
+			Assert.NotNull(actual);
+			Assert.Equal(expected.Count(), actual.Count());
+			Assert.Equal(expected[0].TokenType, actual.First().TokenType);
+			Assert.Equal(expected[0].Value, actual.First().Value);
+		}
+
+		[Fact]
+		public void TryWrite_FutureLocalTimeZone_ConvertsToUtcAndWritesAsUtc()
+		{
+			var input = new DateTime(2099, 12, 31, 23, 59, 59, 999, DateTimeKind.Local);
+
+			var expected = new[]
+				{
+					JsonGrammar.TokenString(@"\/Date(4102473599999)\/")
+				};
+
+			IEnumerable<Token<JsonTokenType>> actual;
+			Assert.True(new MSAjaxDateFilter().TryWrite(new DataWriterSettings(), input, out actual));
+
+			Assert.NotNull(actual);
+			Assert.Equal(expected.Count(), actual.Count());
+			Assert.Equal(expected[0].TokenType, actual.First().TokenType);
+			Assert.Equal(expected[0].Value, actual.First().Value);
+		}
+
+		[Fact]
+		public void TryWrite_FutureUnspecifiedTimeZone_DirectlyWritesAsUtc()
+		{
+			var input = new DateTime(2099, 12, 31, 23, 59, 59, 999, DateTimeKind.Unspecified);
+
+			var expected = new[]
+				{
+					JsonGrammar.TokenString(@"\/Date(4102444799999)\/")
+				};
+
+			IEnumerable<Token<JsonTokenType>> actual;
+			Assert.True(new MSAjaxDateFilter().TryWrite(new DataWriterSettings(), input, out actual));
+
+			Assert.NotNull(actual);
+			Assert.Equal(expected.Count(), actual.Count());
+			Assert.Equal(expected[0].TokenType, actual.First().TokenType);
+			Assert.Equal(expected[0].Value, actual.First().Value);
+		}
+
+		[Fact]
+		public void TryWrite_DateTimeMinValue_DirectlyWritesAsUtc()
+		{
+			var input = DateTime.MinValue;
+
+			var expected = new[]
+				{
+					JsonGrammar.TokenString(@"\/Date(-62135596800000)\/")
+				};
+
+			IEnumerable<Token<JsonTokenType>> actual;
+			Assert.True(new MSAjaxDateFilter().TryWrite(new DataWriterSettings(), input, out actual));
+
+			Assert.NotNull(actual);
+			Assert.Equal(expected.Count(), actual.Count());
+			Assert.Equal(expected[0].TokenType, actual.First().TokenType);
+			Assert.Equal(expected[0].Value, actual.First().Value);
+		}
+
+		[Fact]
+		public void TryWrite_DateTimeMaxValue_DirectlyWritesAsUtc()
+		{
+			var input = DateTime.MaxValue;
+
+			var expected = new[]
+				{
+					JsonGrammar.TokenString(@"\/Date(253402300800000)\/")
+				};
+
+			IEnumerable<Token<JsonTokenType>> actual;
+			Assert.True(new MSAjaxDateFilter().TryWrite(new DataWriterSettings(), input, out actual));
+
+			Assert.NotNull(actual);
+			Assert.Equal(expected.Count(), actual.Count());
+			Assert.Equal(expected[0].TokenType, actual.First().TokenType);
+			Assert.Equal(expected[0].Value, actual.First().Value);
+		}
+
+		#endregion TryWrite Tests
+	}
+}
--- src/JsonFx.Json/Json/Filters/MSAjaxDateFilter.cs
+++ src/JsonFx.Json/Json/Filters/MSAjaxDateFilter.cs
@@ -49,12 +49,14 @@ namespace JsonFx.Json.Filters
 		#region Constant
 
 		private static readonly DateTime EcmaScriptEpoch = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+		private static readonly long MinValueMilliseconds = -62135596800000L;
+		private static readonly long MaxValueMilliseconds = 253402300800000L;
 
-		private const string DateCtorPattern = @"^\\/Date\((\d+?)\)\\/$";
-		private static readonly Regex DateCtorRegex = new Regex(DateCtorPattern, RegexOptions.Compiled|RegexOptions.CultureInvariant|RegexOptions.ECMAScript);
+		private const string MSAjaxDatePattern = @"^\\/Date\(([+\-]?\d+?)\)\\/$";
+		private static readonly Regex MSAjaxDateRegex = new Regex(MSAjaxDatePattern, RegexOptions.Compiled|RegexOptions.CultureInvariant|RegexOptions.ECMAScript);
 
-		private const string DateCtorPrefix = @"\/Date(";
-		private const string DateCtorSuffix = @")\/";
+		private const string MSAjaxDatePrefix = @"\/Date(";
+		private const string MSAjaxDateSuffix = @")\/";
 
 		#endregion Constant
 
@@ -62,11 +64,24 @@ namespace JsonFx.Json.Filters
 
 		public override bool TryRead(DataReaderSettings settings, IEnumerable<Token<JsonTokenType>> tokens, out DateTime value)
 		{
-			// TODO: determine MoveNext or not?
-			Token<JsonTokenType> token = tokens.GetEnumerator().Current;
+			IEnumerator<Token<JsonTokenType>> enumerator = tokens.GetEnumerator();
+			if (!enumerator.MoveNext())
+			{
+				value = default(DateTime);
+				return false;
+			}
 
-			string date = Convert.ToString(token.Value, CultureInfo.InvariantCulture);
-			return this.TryParseMSAjaxDate(date, out value);
+			Token<JsonTokenType> token = enumerator.Current;
+			if (enumerator.Current == null ||
+				enumerator.Current.TokenType != JsonTokenType.String)
+			{
+				value = default(DateTime);
+				return false;
+			}
+
+			return this.TryParseMSAjaxDate(
+				Convert.ToString(token.Value, CultureInfo.InvariantCulture),
+				out value);
 		}
 
 		public override bool TryWrite(DataWriterSettings settings, DateTime value, out IEnumerable<Token<JsonTokenType>> tokens)
@@ -86,7 +101,7 @@ namespace JsonFx.Json.Filters
 		/// <summary>
 		/// Converts an ASP.NET Ajax date string to the corresponding DateTime representation
 		/// </summary>
-		/// <param name="date">Date constructor string</param>
+		/// <param name="date">ASP.NET Ajax date string</param>
 		/// <param name="value"></param>
 		/// <returns>true if parsing was successful</returns>
 		private bool TryParseMSAjaxDate(string date, out DateTime value)
@@ -97,12 +112,12 @@ namespace JsonFx.Json.Filters
 				return false;
 			}
 
-			Match match = MSAjaxDateFilter.DateCtorRegex.Match(date);
+			Match match = MSAjaxDateFilter.MSAjaxDateRegex.Match(date);
 
 			long ticks;
 			if (!match.Success ||
 				!Int64.TryParse(
-					match.Value,
+					match.Groups[1].Value,
 					NumberStyles.Integer,
 					CultureInfo.InvariantCulture,
 					out ticks))
@@ -111,7 +126,18 @@ namespace JsonFx.Json.Filters
 				return false;
 			}
 
-			value = MSAjaxDateFilter.EcmaScriptEpoch.AddMilliseconds(ticks);
+			if (ticks <= MSAjaxDateFilter.MinValueMilliseconds)
+			{
+				value = DateTime.MinValue;
+			}
+			else if (ticks >= MSAjaxDateFilter.MaxValueMilliseconds)
+			{
+				value = DateTime.MaxValue;
+			}
+			else
+			{
+				value = MSAjaxDateFilter.EcmaScriptEpoch.AddMilliseconds(ticks);
+			}
 			return true;
 		}
 
@@ -128,7 +154,7 @@ namespace JsonFx.Json.Filters
 				value = value.ToUniversalTime();
 			}
 
-			// find the time since Jan 1, 1970
+			// find the duration since Jan 1, 1970
 			TimeSpan duration = value.Subtract(MSAjaxDateFilter.EcmaScriptEpoch);
 
 			// get the total milliseconds
@@ -136,9 +162,9 @@ namespace JsonFx.Json.Filters
 
 			// write out as a pseudo Date constructor
 			return String.Concat(
-				MSAjaxDateFilter.DateCtorPrefix,
+				MSAjaxDateFilter.MSAjaxDatePrefix,
 				ticks,
-				MSAjaxDateFilter.DateCtorSuffix);
+				MSAjaxDateFilter.MSAjaxDateSuffix);
 		}
 
 		#endregion Utility Methods
