513bac7eba4c52a4d187c12ad1d6dfb0810b3209
Stephen M. McKamey <stephen@mckamey.com>
2010-07-13 02:49:55 +0000

    - removing JSON specific constants

--- src/JsonFx.Json/Serialization/DataGrammar.cs
+++ src/JsonFx.Json/Serialization/DataGrammar.cs
@@ -69,39 +69,5 @@ namespace JsonFx.Serialization
 		}
 
 		#endregion Reusable Tokens
-
-		#region Keyword Literals
-
-		public const string KeywordUndefined = "undefined";
-		public const string KeywordNull = "null";
-		public const string KeywordFalse = "false";
-		public const string KeywordTrue = "true";
-		public const string KeywordNaN = "NaN";
-		public const string KeywordInfinity = "Infinity";
-
-		#endregion Keyword Literals
-
-		#region Operators
-
-		public const char OperatorArrayBegin = '[';
-		public const char OperatorArrayEnd = ']';
-		public const char OperatorObjectBegin = '{';
-		public const char OperatorObjectEnd = '}';
-		public const char OperatorValueDelim = ',';
-		public const char OperatorPairDelim = ':';
-
-		public const char OperatorStringDelim = '"';
-		public const char OperatorStringDelimAlt = '\'';
-		public const char OperatorCharEscape = '\\';
-
-		public const char OperatorUnaryMinus = '-';
-		public const char OperatorUnaryPlus = '+';
-		public const char OperatorDecimalPoint = '.';
-
-		public const string OperatorCommentBegin = "/*";
-		public const string OperatorCommentEnd = "*/";
-		public const string OperatorCommentLine = "//";
-
-		#endregion Operators
 	}
 }
