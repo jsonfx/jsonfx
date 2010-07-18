cd277096d9fdeea6277d80f2df158f36f47f846d
Stephen M. McKamey <stephen@mckamey.com>
2010-07-18 15:16:12 +0000

    - tweaking comments

--- src/JsonFx/Common/CommonTokenType.cs
+++ src/JsonFx/Common/CommonTokenType.cs
@@ -41,7 +41,7 @@ namespace JsonFx.Common
 		/// No token
 		/// </summary>
 		/// <remarks>
-		/// The token Value and Name must be null.
+		/// The <see cref="Token<CommonTokenType>.Value"/> and <see cref="Token<CommonTokenType>.Name"/> must be left empty.
 		/// Represents the absence of a token and is not represented in the document.
 		/// </remarks>
 		None,
@@ -50,8 +50,8 @@ namespace JsonFx.Common
 		/// Begin object, the token contains the object Name
 		/// </summary>
 		/// <remarks>
-		/// In the absence of a object Name, one may be automatically chosen.
-		/// The object Name is often not represented directly in the output as some formats do not have the concept of named objects.
+		/// In the absence of an object <see cref="Token<CommonTokenType>.Name"/>, one may be automatically chosen.
+		/// The object <see cref="Token<CommonTokenType>.Name"/> is often not represented directly in the output as some formats do not have the concept of named objects.
 		/// Marks the beginning of an associative array of key-value pairs
 		/// </remarks>
 		ObjectBegin,
@@ -60,7 +60,8 @@ namespace JsonFx.Common
 		/// End object
 		/// </summary>
 		/// <remarks>
-		/// The token Value and Name must be null. The object Name is determined by the ObjectBegin token.
+		/// The <see cref="Token<CommonTokenType>.Value"/> and <see cref="Token<CommonTokenType>.Name"/> must be null.
+		/// The object <see cref="Token<CommonTokenType>.Name"/> is determined by the <see cref="CommonTokenType.ObjectBegin"/> token.
 		/// Marks the end of the last object property as well as the object itself
 		/// </remarks>
 		ObjectEnd,
@@ -69,8 +70,8 @@ namespace JsonFx.Common
 		/// Begin array, the token contains the array Name
 		/// </summary>
 		/// <remarks>
-		/// In the absence of an array Name, one may be automatically chosen.
-		/// The array Name is often not represented directly in the output as some formats do not have the concept of named arrays.
+		/// In the absence of an array <see cref="Token<CommonTokenType>.Name"/>, one may be automatically chosen.
+		/// The array <see cref="Token<CommonTokenType>.Name"/> is often not represented directly in the output as some formats do not have the concept of named arrays.
 		/// Marks the beginning of a single-dimensional array of values
 		/// </remarks>
 		ArrayBegin,
@@ -79,7 +80,8 @@ namespace JsonFx.Common
 		/// End array
 		/// </summary>
 		/// <remarks>
-		/// The token Value and Name must be null. The array Name is determined by the ObjectBegin token.
+		/// The <see cref="Token<CommonTokenType>.Value"/> and <see cref="Token<CommonTokenType>.Name"/> must be null.
+		/// The array <see cref="Token<CommonTokenType>.Name"/> is determined by the <see cref="CommonTokenType.ArrayBegin"/> token.
 		/// Marks the end of the last array item as well as the array itself
 		/// </remarks>
 		ArrayEnd,
@@ -88,7 +90,7 @@ namespace JsonFx.Common
 		/// Begin object property, the token contains the property Name
 		/// </summary>
 		/// <remarks>
-		/// The property Name is required as all formats name their properties.
+		/// The <see cref="Token<CommonTokenType>.Name"/> is required as all formats name their properties.
 		/// Marks the beginning of an object property.
 		/// </remarks>
 		Property,
@@ -97,9 +99,9 @@ namespace JsonFx.Common
 		/// Simple primitive value, the token contains the primitive value
 		/// </summary>
 		/// <remarks>
-		/// The token Value is optional, i.e. null is a valid primitive value.
-		/// Typically token Value is serialized directly as a single primitive.
-		/// If the token value is not a CLR primitive, then the value must implement IConvertable, IFormatable or be meaningful when cast to a string.
+		/// The <see cref="Token<CommonTokenType>.Value"/> is optional, i.e. null is a valid primitive value.
+		/// Typically <see cref="Token<CommonTokenType>.Value"/> is serialized directly as a single primitive.
+		/// If the <see cref="Token<CommonTokenType>.Value"/> is not a CLR primitive, then the value must implement IConvertable, IFormatable or be meaningful when cast to a string.
 		/// </remarks>
 		Primitive,
 
@@ -107,7 +109,7 @@ namespace JsonFx.Common
 		/// Value delimiter
 		/// </summary>
 		/// <remarks>
-		/// The token Value and Name must be null.
+		/// The <see cref="Token<CommonTokenType>.Value"/> and <see cref="Token<CommonTokenType>.Name"/> must be left empty.
 		/// Marks the end of an object property or array item
 		/// </remarks>
 		ValueDelim
